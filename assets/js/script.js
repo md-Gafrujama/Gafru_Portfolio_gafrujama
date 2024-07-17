03961b1b98b1faa1426b6007e08488645b6e1e3c
MD GAFRUJAMA ANSARI <156898012+md-Gafrujama@users.noreply.github.com>
2024-07-17 17:48:56 +0530

    comit changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: ["frontend developer", "backend developer", "software developer", "system analyst","freelancer"],
+    strings: ["frontend developer", "backend developer", "software developer", "freelancer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
